bc842b4ca321425bdb18cf6294e50278c633b81f
bahman <bahman026@gmail.com>
2023-10-17 14:29:32 +0330

    create article model

---
+++ app/Models/Article.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Article extends Model
+{
+    use SoftDeletes;
+    use HasFactory;
+
+    /**
+     * @return BelongsTo
+     */
+    public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
