c6f06899f95226bc815887b095d4baefce87e289
bahman <bahman026@gmail.com>
2023-10-22 12:05:31 +0330

    update ImageFactory.php

--- database/factories/ImageFactory.php
+++ database/factories/ImageFactory.php
@@ -18,7 +18,7 @@ class ImageFactory extends Factory
     public function definition(): array
     {
         return [
-            'path' => fake()->imageUrl,
+            'path' => 'ArticleDefault.jpg',
             'created_at' => now(),
         ];
     }
